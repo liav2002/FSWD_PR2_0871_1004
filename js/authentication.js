4878feadd128d6946859646311adcfcae346c8c1
liav2002 <liavmail00@gmail.com>
2024-02-06 16:49:14 +0200

    full backend complete

--- js/authentication.js
+++ js/authentication.js
@@ -3,27 +3,33 @@ function login() {
     var password = document.getElementById('password').value;
 
     // Retrieve users from local storage
-    const users = JSON.parse(localStorage.getItem('users')) || [];
+    let users = JSON.parse(localStorage.getItem('users')) || [];
 
     // Check if the provided username and password match any user
-    var user = users.find(u => u.username === username && u.password === password);
+    let user = users.find(u => u.username === username && u.password === password);
 
     if (user) {
         if (user.block === "false") {
             // Update local storage with the logged-in user's data
-            const loggedInUserData = {
+            let loggedInUserData = {
                 "user_id": user.user_id,
+                "admin": user.admin,
                 "date-last-login": new Date().toISOString()
             };
 
             // Record user details in local storage
             writeUserLoggedInData(loggedInUserData);
 
+            // Update last login time in users
+            user["date-last-login"] = loggedInUserData["date-last-login"];
+
+            // Save updated users data to local storage
+            localStorage.setItem('users', JSON.stringify(users));
+
             alert('Login successful!');
 
-            // redirect to home.html
+            // Redirect to home.html
             window.location.href = './home.html';
-
         } else {
             alert('Account is blocked. Contact support.');
         }
@@ -31,7 +37,6 @@ function login() {
         alert('Invalid username or password.');
     }
 }
-
 // Function to write data to local storage
 function writeUserLoggedInData(loggedInUserData) {
     localStorage.setItem('loggedInUser', JSON.stringify(loggedInUserData));
